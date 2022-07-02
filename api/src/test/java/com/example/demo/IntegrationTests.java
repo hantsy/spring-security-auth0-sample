12c4f9e4601624ae14b454c2ebf96a465130f37b
hantsy <hantsy@gmail.com>
2022-07-02 17:27:31 +0800

    chore: clean codes

--- api/src/test/java/com/example/demo/IntegrationTests.java
+++ api/src/test/java/com/example/demo/IntegrationTests.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.notNullValue;
